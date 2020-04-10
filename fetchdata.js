1e25d1d15e5ec54dafbadd8521cf304aa0c9ad0f
Frederick-88 <frederickfd88@gmail.com>
2020-04-10 23:34:55 +0700

    PUSHH MALAM

--- fetchdata.js
+++ fetchdata.js
@@ -19,36 +19,52 @@
 // })
 
 // PAKAI AXIOS. --- EASIER
+// FUNCTION TAMBAH 
 
-axios.get('http://35.240.201.155:3000/api/v1/wanderlink/show/idn/destination')
+const getData = () => {
+    axios.get('http://35.240.201.155:3000/api/v1/wanderlink/show/idn/destination')
 
-    .then((respond) => {
-        console.log(respond.data);
-
-        // TAMPILAN HTML
-        for (let i = 10; i <= 20; i++) {
-            displayISI(respond.data[i]);
-
-        }
-    })
+        .then((respond) => {
+            console.log(respond.data);
+            const dimulai = document.querySelectorAll(".pantai").length
+            const sampaiKe = 6 + dimulai
 
 
-    .catch((error) => {
-        console.error(error);
+            // TAMPILAN HTML
+            for (let i = dimulai; i < sampaiKe; i++) {
+                displayISI(respond.data[i]);
+            }
 
-    })
+            // for (let i = 13; i >=13; i++) {
+            //     displayISI2(respond.data[i]);
+            // }
+
+        })
+
+
+        .catch((error) => {
+            console.error(error);
+
+        })
+}
 
 
 const displayISI = (item) => {
-    const imgURL= 'http://35.240.201.155:3000'
+    const imgURL = 'http://35.240.201.155:3000'
     let tujuan = document.getElementById("displayHTML")
     let isiHTML =
-        `<div class="col-sm-3">
+        `<div class="col-sm-4 mb-5 pantai">
 
-        <div class="card" style="width: 18rem;">
+        <div class="card">
   <img src="${imgURL}/${item.images}" class="card-img-top" alt="..." >
   <div class="card-body">
-    <p class="card-text">${item.address}</p>
+    <p class="card-text">
+    <b>Destination Name :</b> ${item.destinationName}
+    <br>
+    <b>City</b> : ${item.city}
+    <br>
+    <b>Address</b> :${item.address}
+    </p>
   </div>
 </div>
     
@@ -56,3 +72,6 @@ const displayISI = (item) => {
 
     tujuan.innerHTML += isiHTML
 }
+
+
+getData()
